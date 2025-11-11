2bc1362ac7a74acaacfe7fd8d7fc10f6099c0c41
Aditya Chavan <chavanaditya9325@gmail.com>
2025-11-11 18:40:06 +0530

    Refactor App component to manage theme state and apply dark mode; enhance index.css with additional styles for background and text effects

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,10 +6,35 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  const [theme, setTheme] = useState(() => {
+    try {
+      return localStorage.getItem("theme") || "dark";
+    } catch {
+      return "dark";
+    }
+  })
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === 'dark') {
+      root.classList.add("dark");
+    }else {
+      root.classList.remove("dark");
+    }
+    try { 
+      localStorage.setItem("theme", theme);
+    } catch{}
+  }, [theme]);
+
   return (
-    <>
-     <h1 className='bg-black text-white'>Hi there </h1>
-    </>
+    <div className="flex flex-col min-h-screen antialiased">
+      <div id="__grain" aria-hidden="true" className=""></div>
+      {/* <Navbar theme={theme} setTheme={setTheme} />
+      <main className="flex-grow">
+        <Home />
+      </main>
+      <Footer /> */}
+    </div>
   )
 }
 
