f57a6b95eecc6a05519ba71ef1947f89e6ccfa76
Aditya Chavan <chavanaditya9325@gmail.com>
2025-11-30 19:13:14 +0530

    Implement login functionality with error handling and token generation

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -51,6 +51,45 @@ export const registerUser = asyncHandler(async(req, res, next) => {
 
 
 export const login = asyncHandler(async(req, res, next) => {
+   try {
+     const {email, password } = req.body;
+ 
+     if (!email || !password) {
+         return res.status(400).json({meassage: "All fields are required"})
+     }
+ 
+     const user = await User.findOne({email}).select("+password");
+ 
+     if(!user) return res.status(400).json({message: "User does not exist"})
+ 
+     const isMatch = await bcrypt.compare(password, user.password);
+ 
+     if(!isMatch){
+         return res.status(400).json({message: "email or password is incorrect"})
+     }
+     
+     const token = generateToken(user._id);
+ 
+     return res 
+         .status(200)
+         .cookie("token", token, {
+             httpOnly:true, 
+             secure: process.env.NODE_ENV === "production",
+             sameSite: "strict",
+             maxAge: 7 * 24 * 60 * 1000,
+         })
+         .json({
+             success: true,
+             message: "Logged in successfully",
+             user: {
+                 id: user._id,
+                 name: user.name,
+                 email: user.email,
+             },
+         });
+   } catch (error) {
+        next(error);
+   }
 
 })
 
