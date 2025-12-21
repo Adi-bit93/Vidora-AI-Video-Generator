c2c951ebb6feaf5d9ff5c9aeacd1ef8881252247
Aditya Chavan <chavanaditya9325@gmail.com>
2025-12-21 10:55:12 +0530

    Refactor video routes to consolidate controller imports for improved organization

--- Backend/src/routes/video.routes.js
+++ Backend/src/routes/video.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { upload } from '../middlewares/upload.middleware.js';
-import { uploadVideo } from '../controllers/video.controller.js';
+import { getMyVideos, getVideoById, saveVideo, uploadVideo } from '../controllers/video.controller.js';
 import { requireAuth } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -11,5 +11,14 @@ router.post(
     upload.single("video"),
     uploadVideo
 )
+
+// save video to metadata
+router.post("/save", requireAuth, saveVideo)
+
+// get all the videos of user 
+router.get("/me", requireAuth, getMyVideos)
+
+// get video by Id
+router.get("/:id", requireAuth, getVideoById)
 
 export default router;
